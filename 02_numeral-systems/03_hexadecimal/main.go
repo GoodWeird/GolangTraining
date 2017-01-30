bebd710403ecf4300f8e47a30fe053800946db7d
rafiyqw <rafiyqw@gmail.com>
2017-01-30 19:55:05 +0700

    computer fundamentals

---
+++ 02_numeral-systems/03_hexadecimal/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Printf("%d-%b-%x",42,42,42)
+	//fmt.Printf("%d-%b-%#x",42,42,42)
+	fmt.Printf("%d-%b-%#X",42,42,42)
+	//fmt.Printf("%d \t %b \t %#X",42,42,42)
+}
